87972665de7a592ac36596c0fb5900e0697a44c6
scottyb34 <scottyb34@hotmail.com>
2016-07-06 15:40:56 -0600

    Update SearchSort.java

--- src/SearchSort.java
+++ src/SearchSort.java
@@ -3,25 +3,17 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class SearchSort {
+    //explained in readme.  one solution uses a boolean to check if a swap has been completed or not.  
+    //This does not have to be recursive.  Otherwise this method is explained in the readme.
     public static void bubbleSort(String[] arr) {
-        boolean made_a_swap = true;
-        while(made_a_swap) {
-            made_a_swap = false;
-            for(int i = 0; i < arr.length - 1; ++i) {
-                if(arr[i].compareTo(arr[i + 1]) > 0) {
-                    String t = arr[i+1];
-                    arr[i+1] = arr[i];
-                    arr[i] = t;
-                    made_a_swap = true;
-                }
-            }
-        }
+       
+              
     }
 
     // takes a sorted array and returns the index of the desired string or -1 if not found
     public static int binarySearch(String[] arr, String lookfor) {
         //call binarySearchHelper
-        return binarySearchHelper(arr, 0, arr.length, lookfor);
+        return binarySearchHelper(//what goes in here?);
     }
         
     private static int binarySearchHelper(String[] arr, int lo, int hi, String lookfor) {
@@ -31,79 +23,44 @@ public class SearchSort {
         //What are your base cases?  Handle the case where the string is not found.
         //Part of the trick here is you will be  maintaining the original array.   You are just moving the pointers "lo" and "hi"
         //so that the function looks at a specific part of the array.  This way you will be able to return the index of "lookfor".
-        if(lo >= hi)
-            return -1;
-        int mid = (hi + lo) / 2;
-        int c = arr[mid].compareTo(lookfor);
-        if(c == 0)
-            return mid;
-        if(c < 0)
-            return binarySearchHelper(arr, mid + 1, hi, lookfor);
-        else
-            return binarySearchHelper(arr, lo, mid, lookfor);
+        return null;
     }
 
     public static String[] mergeSort(String[] arr) {
-        if(arr.length == 1)
-            return arr;
-        int mid = arr.length / 2;
-        String[] first = mergeSort(Arrays.copyOfRange(arr, 0, mid));
-        String[] second = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
-        return merge(first, second);
+        //check and make sure array has more than one item. 
+        //split the array into two eqal parts
+        //recursevely call mergeSort with both of your new arrays
+        //use merge to put the two arrays back together.
+        
+        //This is not easy, don't be sureprised if you are frustrated
+        //Feel free to look up some solutions on line.
+        //you can do it.
+        
     }
 
     private static String[] merge(String[] a, String[] b) {
-        int anext = 0, bnext = 0, rnext = 0;
-        String[] ret = new String[a.length + b.length];
-
-        while(anext < a.length || bnext < b.length) {
-            String as = anext < a.length ? a[anext] : null;
-            String bs = bnext < b.length ? b[bnext] : null;
-            if(as == null) {
-                ret[rnext] = bs;
-                bnext++;
-            }
-            else if(bs == null) {
-                ret[rnext] = as;
-                anext++;
-            }
-            else if(as.compareTo(bs) < 0) {
-                ret[rnext] = as;
-                anext++;
-            }
-            else {
-                ret[rnext] = bs;
-                bnext++;
-            }
-            rnext++;
-        }
-        return ret;
+        //takes two unsorted string arrays and makes them one sorted string array. 
+        //ie: a = t and b = z results in an array of t,z
+        //ie: a = a,z,f and b = b,c,h results in an array of a,b,c,f,h,z
+        
     }
-
     public static void quickSort(String[] arr) {
         // be brave, use google
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner sc = new Scanner(new File(args[0]));
-        List<String> names = new ArrayList<String>();
-        while(sc.hasNextLine())
-            names.add(sc.nextLine());
-        String[] arr = names.toArray(new String[0]);
-
-
-        System.out.println(Arrays.toString(arr));
-        bubbleSort(arr);
-        System.out.println(Arrays.toString(arr));
-
-
-        System.out.println(binarySearch(arr, "Chewbacca"));
-        System.out.println(binarySearch(arr, "Chewbaccawww"));
-
-
-        String[] arr2 = names.toArray(new String[0]);
-        arr2 = mergeSort(arr2);
-        System.out.println(Arrays.toString(arr2));
+        //First make this compile.
+        
+        //read in a file from args[0] and store in an array
+        //write bubble sort and test, by printing our your new array
+        //before and after calling bubbleSort
+        
+        //Use binarySearch to search for Luke Skywalker, I found him at index 16.
+        //test binarySearch with a name that is not in the array.
+        
+        //test merge sort with a fresh, unsorted copy, of the array 
+        //you made from the original data.
+        
 
     }
 }
